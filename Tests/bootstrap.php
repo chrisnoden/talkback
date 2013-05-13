23eaa72c65f6877d8ff8be88b7844583dbc5c3f5
Chris Noden <chris@noden.net>
2013-05-13 09:54:48 +0100

    Organising structure

---
+++ Tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Chris Noden using JetBrains PhpStorm.
+ *
+ * @author Chris Noden, @chrisnoden
+ * @copyright (c) 2009 to 2013 Chris Noden
+ */
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Run "composer install --dev" to create autoloader.');
+}
+
+$loader = require $autoloadFile;
+$loader->add('Talkback\Tests', __DIR__);
