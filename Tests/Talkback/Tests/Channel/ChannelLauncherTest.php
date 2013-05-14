750d0aa5d60897848e3d329c0c88d4419c3c985a
Chris Noden <chris@noden.net>
2013-05-14 17:36:38 +0100

    Refactoring tests

---
+++ Tests/Talkback/Tests/Channel/ChannelLauncherTest.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Created by Chris Noden using JetBrains PhpStorm.
+ * 
+ * @author Chris Noden, @chrisnoden
+ * @copyright (c) 2009 to 2013 Chris Noden
+ */
+
+namespace Talkback\Tests\Talkback;
+
+use Talkback\Channel\ChannelLauncher;
+use Talkback\Channel\Html;
+use Talkback\Logger;
+use Talkback\Router;
+
+class ChannelLauncherTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $_testFileName;
+    private $_invalidFileName;
+
+
+    public function setUp() {
+        $this->_testFileName = '/private/tmp/test.log';
+        $this->_invalidFileName = DIRECTORY_SEPARATOR . 'invalidpath' . DIRECTORY_SEPARATOR . 'file.log';
+
+        if (file_exists($this->_testFileName)) {
+            unlink($this->_testFileName);
+        }
+        if (file_exists($this->_invalidFileName)) {
+            unlink($this->_invalidFileName);
+        }
+    }
+
+
+    public function tearDown()
+    {
+        if (file_exists($this->_testFileName)) {
+            unlink($this->_testFileName);
+        }
+        if (file_exists($this->_invalidFileName)) {
+            unlink($this->_invalidFileName);
+        }
+    }
+
+
+    /**
+     * Tests the File Channel
+     * Ensures the target File does not exist until we first write to it
+     * Then asserts that the contents of the log file are what we expect
+     */
+    public function testFileChannel()
+    {
+        $obj = ChannelLauncher::File($this->_testFileName);
+        $this->assertInstanceOf('Talkback\Channel\File', $obj);
+        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
+        $this->assertInstanceOf('Talkback\Object', $obj);
+        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
+        $this->assertFileNotExists($this->_testFileName);
+        $obj->write("Test");
+        $this->assertFileExists($this->_testFileName);
+        $this->assertStringEqualsFile($this->_testFileName, "Test\n");
+    }
+
+
+    /**
+     * Trying to instantiate a new File channel without a filename
+     * or with an invalid filename should cause an Exception to be thrown
+     */
+    public function testFileChannelException()
+    {
+        $this->setExpectedException(
+            'Talkback\Exception\InvalidArgumentException', 'filename must be an absolute filename in a writeable directory'
+        );
+        $obj = ChannelLauncher::File('');
+        $obj = ChannelLauncher::File($this->_invalidFileName);
+    }
+
+
+    /**
+     * Basic assertions of a new Growl Channel object
+     */
+    public function testGrowlChannel()
+    {
+        $obj = ChannelLauncher::Growl('testapp');
+        $this->assertInstanceOf('Talkback\Channel\Growl', $obj);
+        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
+        $this->assertInstanceOf('Talkback\Object', $obj);
+        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
+    }
+
+
+    /**
+     * Basic assertions of a new Syslog Channel object
+     */
+    public function testSyslogChannel()
+    {
+        $obj = ChannelLauncher::Syslog('testapp');
+        $this->assertInstanceOf('Talkback\Channel\Syslog', $obj);
+        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
+        $this->assertInstanceOf('Talkback\Object', $obj);
+        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
+    }
+
+
+    /**
+     * Basic assertions of a new Html Channel object
+     */
+    public function testHtmlChannel()
+    {
+        $obj = new Html();
+        $this->assertInstanceOf('Talkback\Channel\Html', $obj);
+        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
+        $this->assertInstanceOf('Talkback\Object', $obj);
+        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
+        $obj->setBasicClient();
+        $this->expectOutputString("Test\n");
+        $obj->write('Test');
+        $obj->addField('tag');
+        $obj->setFieldValues(array('tag' => 'mytag'));
+        $this->expectOutputString("Test\nmytag Test 2\n");
+        $obj->write('Test 2');
+    }
+
+
+    /**
+     * Basic assertions of a new Prowl Channel object
+     */
+    public function testCommsProwl()
+    {
+        $obj = ChannelLauncher::Prowl('My Test App', 'testapikey');
+        $this->assertInstanceOf('Talkback\Channel\Prowl', $obj);
+        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
+        $this->assertInstanceOf('Talkback\Object', $obj);
+        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
+    }
+}
--- Tests/Talkback/Tests/RouterTest.php
+++ Tests/Talkback/Tests/RouterTest.php
@@ -10,29 +10,11 @@ namespace Talkback\Tests\Talkback;
 
 
 use Psr\Log\LogLevel;
-use Talkback\Channel\ChannelLauncher;
-use Talkback\Channel\Html;
 use Talkback\Logger;
 use Talkback\Router;
 
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
-
-    private $_testFileName;
-    private $_invalidFileName;
-
-
-    public function setUp() {
-        $this->_testFileName = '/private/tmp/test.log';
-        $this->_invalidFileName = DIRECTORY_SEPARATOR . 'invalidpath' . DIRECTORY_SEPARATOR . 'file.log';
-
-        if (file_exists($this->_testFileName)) {
-            unlink($this->_testFileName);
-        }
-        if (file_exists($this->_invalidFileName)) {
-            unlink($this->_invalidFileName);
-        }
-    }
 
 
     /**
@@ -88,6 +70,8 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     /**
      * This chains after the above test to check the returned logger
      * still has the new name - it persisted
+     *
+     * @depends testStaticBinding
      */
     public function testLoggerStaticTest()
     {
@@ -105,109 +89,6 @@ class RouterTest extends \PHPUnit_Framework_TestCase
         $this->expectOutputString("hello, this is a test log\n");
         $obj = Logger::getLogger('test logger');
         $obj->log("hello, this is a test log", LogLevel::ERROR);
-    }
-
-
-    /**
-     * Tests the File Channel
-     * Ensures the target File does not exist until we first write to it
-     * Then asserts that the contents of the log file are what we expect
-     */
-    public function testFileChannel()
-    {
-        $obj = ChannelLauncher::File($this->_testFileName);
-        $this->assertInstanceOf('Talkback\Channel\File', $obj);
-        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
-        $this->assertInstanceOf('Talkback\Object', $obj);
-        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
-        $this->assertFileNotExists($this->_testFileName);
-        $obj->write("Test");
-        $this->assertFileExists($this->_testFileName);
-        $this->assertStringEqualsFile($this->_testFileName, "Test\n");
-    }
-
-
-    /**
-     * Trying to instantiate a new File channel without a filename
-     * or with an invalid filename should cause an Exception to be thrown
-     */
-    public function testFileChannelException()
-    {
-        $this->setExpectedException(
-            'Talkback\Exception\InvalidArgumentException', 'filename must be an absolute filename in a writeable directory'
-        );
-        $obj = ChannelLauncher::File('');
-        $obj = ChannelLauncher::File($this->_invalidFileName);
-    }
-
-
-    /**
-     * Basic assertions of a new Growl Channel object
-     */
-    public function testGrowlChannel()
-    {
-        $obj = ChannelLauncher::Growl('testapp');
-        $this->assertInstanceOf('Talkback\Channel\Growl', $obj);
-        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
-        $this->assertInstanceOf('Talkback\Object', $obj);
-        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
-    }
-
-
-    /**
-     * Basic assertions of a new Syslog Channel object
-     */
-    public function testSyslogChannel()
-    {
-        $obj = ChannelLauncher::Syslog('testapp');
-        $this->assertInstanceOf('Talkback\Channel\Syslog', $obj);
-        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
-        $this->assertInstanceOf('Talkback\Object', $obj);
-        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
-    }
-
-
-    /**
-     * Basic assertions of a new Html Channel object
-     */
-    public function testHtmlChannel()
-    {
-        $obj = new Html();
-        $this->assertInstanceOf('Talkback\Channel\Html', $obj);
-        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
-        $this->assertInstanceOf('Talkback\Object', $obj);
-        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
-        $obj->setBasicClient();
-        $this->expectOutputString("Test\n");
-        $obj->write('Test');
-        $obj->addField('tag');
-        $obj->setFieldValues(array('tag' => 'mytag'));
-        $this->expectOutputString("Test\nmytag Test 2\n");
-        $obj->write('Test 2');
-    }
-
-
-    /**
-     * Basic assertions of a new Prowl Channel object
-     */
-    public function testCommsProwl()
-    {
-        $obj = ChannelLauncher::Prowl('My Test App', 'testapikey');
-        $this->assertInstanceOf('Talkback\Channel\Prowl', $obj);
-        $this->assertInstanceOf('Talkback\Channel\ChannelObject', $obj);
-        $this->assertInstanceOf('Talkback\Object', $obj);
-        $this->assertInstanceOf('Talkback\Channel\Channelinterface', $obj);
-    }
-
-
-    public function tearDown()
-    {
-        if (file_exists($this->_testFileName)) {
-            unlink($this->_testFileName);
-        }
-        if (file_exists($this->_invalidFileName)) {
-            unlink($this->_invalidFileName);
-        }
     }
 
 
