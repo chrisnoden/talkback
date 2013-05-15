3828ba6699fc6323d59a75739c382755009e3ff1
Chris Noden <chris@noden.net>
2013-05-15 23:44:57 +0100

    Added Object class Test

---
+++ Tests/Talkback/Tests/ObjectTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by Chris Noden using JetBrains PhpStorm.
+ * 
+ * @author Chris Noden, @chrisnoden
+ * @copyright (c) 2009 to 2013 Chris Noden
+ */
+
+namespace Talkback\Tests;
+
+use Talkback\Object;
+
+class ObjectTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testObject()
+    {
+        $obj = new Object();
+        $this->assertInstanceOf('Talkback\Object', $obj);
+    }
+
+    public function testObjectReturnValue()
+    {
+        $obj = new Object();
+        $this->assertEquals('Talkback\Object', $obj->__toString());
+    }
+
+}
