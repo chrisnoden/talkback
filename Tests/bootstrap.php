138b27edaa064198bf910e13d06d817da1338477
Chris Noden <chris.noden@gmail.com>
2013-07-19 16:17:45 +0100

    Fix PHP error when running tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,6 +6,10 @@
  * @copyright (c) 2009 to 2013 Chris Noden
  */
 
+if (!ini_get('date.timezone')) {
+    date_default_timezone_set('Europe/London');
+}
+
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Run "composer install --dev" to create autoloader.');
 }
